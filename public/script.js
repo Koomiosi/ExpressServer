1eb02b82c310d95df80eab43cc258c353ca0f00f
Tony Kelanen <tony.kelanen@student.careeria.fi>
2024-10-03 12:46:31 +0300

    Script updates

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ async function henkilosto() {
     var x = `<table><thead><th><font size='4'>Nimi</font></th><th><font size='4'>Status</font></th></thead><tbody>`
 
     try {
-        const response = await fetchth("https://oyyritysab.onrender.com/api/henkilosto")
+        const response = await fetch("/api/henkilosto")
     
         const henkilostodata = await response.json()
     
